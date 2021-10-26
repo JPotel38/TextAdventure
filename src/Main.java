11ab4abbd6cc29dc7f50d4a24a490b76b81facc6
Jérémy Potel <potel.jeremy@gmail.com>
2021-10-26 10:57:40 +0200

    Add fireball spell

--- src/Main.java
+++ src/Main.java
@@ -15,14 +15,22 @@ public class Main {
 
         // Player variables
         int health = 100;
+        int mp = 50;
         int attackDamage = 50;
+        int spellDamage = 80;
         int numHealthPotions = 3;
+        int numManaPotions = 1;
         int healthPotionHealAmount = 30;
         int healthPotionDropChance = 50; // Percentage
+        int manaPotionDropChance = 30; // Percentage
+        int manaPotionRegenAmount = 15;
 
         boolean running = true;
 
-        System.out.println("Welcome to the Dungeon !");
+        System.out.println("""
+                As a mercenary, you were hired to clean the local evil dungeon.
+                'Time to enter it, I guess', you think yourself.
+                An enemy is already waiting for you. It wilb be an amazing day !""");
 
         GAME:
         while (running) {
@@ -34,42 +42,78 @@ public class Main {
 
             while (enemyHealth > 0) {
                 System.out.println("\t#Your HP: " + health);
+                System.out.println("\t#Your MP: " + mp);
                 System.out.println("\t#" + enemy + "'s HP: " + enemyHealth);
                 System.out.println("\n\tWhat would you like to do ?");
                 System.out.println("\t1. Attack");
                 System.out.println("\t2. Drink health potion");
-                System.out.println("\t3. Run !");
+                System.out.println("\t3. Drink mana potion");
+                System.out.println("\t4. Run !");
 
                 String input = in.nextLine();
-                if (input.equals("1")) {
-                    int damageDealt = rand.nextInt(attackDamage);
-                    int damageTaken = rand.nextInt(enemyAttackDamage);
+                switch (input) {
+                    case "1":
+                        System.out.println("\t1. Use weapon.");
+                        System.out.println("\t2. Use fireball (10 mp).");
+                        input = in.nextLine();
+                        if (input.equals("1")) {
+                            int damageDealt = rand.nextInt(attackDamage);
+                            int damageTaken = rand.nextInt(enemyAttackDamage);
 
-                    enemyHealth -= damageDealt;
-                    health -= damageTaken;
+                            enemyHealth -= damageDealt;
+                            health -= damageTaken;
 
-                    System.out.println("\t> You strike the " + enemy + " for " + damageDealt + " damage.");
-                    System.out.println("\t> You receive " + damageTaken + " in retaliation.");
+                            System.out.println("\t> You strike the " + enemy + " for " + damageDealt + " damage.");
+                            System.out.println("\t> You receive " + damageTaken + " in retaliation.");
 
-                    if (health < 1) {
-                        System.out.println("\t> You have taken too much damage !");
-                    }
-                } else if (input.equals("2")) {
-                    if (numHealthPotions > 0) {
-                        health += healthPotionHealAmount;
-                        numHealthPotions--;
-                        System.out.println("\t> You drink a health potion, healing yourself for " + healthPotionHealAmount + " hp." +
-                                "\n\t> You now have " + health + " hp !" +
-                                "\n\t> You have " + numHealthPotions + " health potions left. \n");
-                    } else {
-                        System.out.println("You have no health potions left ! Defeat enemies to have a chance to het one !");
-                    }
+                            if (health < 1) {
+                                System.out.println("\t> You have taken too much damage !");
+                            }
+                        } else if (input.equals("2")) {
+                            int damageDealt = rand.nextInt(spellDamage);
+                            int damageTaken = rand.nextInt(enemyAttackDamage);
+                            enemyHealth -= damageDealt;
+                            health -= damageTaken;
+                            mp -= 10;
 
-                } else if (input.equals("3")) {
-                    System.out.println("you run away from the " + enemy + " !");
-                    continue GAME;
-                } else {
-                    System.out.println("\tInvalid command !");
+                            System.out.println("\t> You burn the " + enemy + " for " + damageDealt + " damage.");
+                            System.out.println("\t> You receive " + damageTaken + " in retaliation.");
+
+                            System.out.println("\t> You lose 10 mp. You have " + mp + " left.");
+
+                            if (health < 1) {
+                                System.out.println("\t> You have taken too much damage !");
+                            }
+                        }
+                        break;
+                    case "2":
+                        if (numHealthPotions > 0) {
+                            health += healthPotionHealAmount;
+                            numHealthPotions--;
+                            System.out.println("\t> You drink a health potion, healing yourself for " + healthPotionHealAmount + " hp." +
+                                    "\n\t> You now have " + health + " hp !" +
+                                    "\n\t> You have " + numHealthPotions + " health potions left. \n");
+                        } else {
+                            System.out.println("You have no health potions left ! Defeat enemies to have a chance to get one !");
+                        }
+                        break;
+                    case "3":
+                        if (numManaPotions > 0) {
+                            mp += manaPotionRegenAmount;
+                            numManaPotions--;
+                            System.out.println("\t> You drink a mana potion, regen your magic for " + manaPotionRegenAmount + " mp." +
+                                    "\n\t> You now have " + mp + " mp !" +
+                                    "\n\t> You have " + numManaPotions + " mana potions left. \n");
+                        } else {
+                            System.out.println("You have no mana potions left ! Defeat enemies to have a chance to get one !");
+                        }
+                        break;
+                    case "4":
+                        System.out.println("you run away from the " + enemy + " !");
+                        continue GAME;
+                    default:
+                        System.out.println("\tInvalid command !");
+                        break;
                 }
             }
             if (health < 1) {
@@ -83,6 +127,11 @@ public class Main {
                 numHealthPotions++;
                 System.out.println(" # The " + enemy + " dropped a health potion! #");
                 System.out.println((" # You now have " + numHealthPotions + " health potion(s). # "));
+            }
+            if (rand.nextInt(100) < manaPotionDropChance) {
+                numManaPotions++;
+                System.out.println(" # The " + enemy + " dropped a mana potion! #");
+                System.out.println((" # You now have " + numManaPotions + " mana potion(s). # "));
             }
             System.out.println("-----------------------------------------------");
             System.out.println("What would you like to do now ?");
@@ -98,7 +147,7 @@ public class Main {
 
             if (input.equals("1")) {
                 System.out.println("You continue your adventure !");
-            } else if (input.equals("2")) {
+            } else {
                 System.out.println("You exit the dungeon, successful from your adventure!");
                 break;
             }
